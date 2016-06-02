8315693dc485c6d1629d20f1bfc9439e0c4e69ac
Andrea <Andrea@Andrea-PC>
2016-06-02 17:37:56 +0200

    messo la prima classe con solo il main per vedere se riuscivo a scrivere un record

---
+++ src/main/java/com/mycompany/gestionedatabase/ProvaJpa.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gestionedatabase;
+
+import java.util.List; 
+import javax.persistence.EntityManager; 
+import javax.persistence.EntityManagerFactory; 
+import javax.persistence.Persistence; 
+import entity.TAnagrafica;
+ 
+
+/**
+ *
+ * @author Andrea
+ */
+public class ProvaJpa {
+    
+    public static void main(String[] args) {
+        
+       EntityManagerFactory emf = Persistence.createEntityManagerFactory("per_dbprova");
+       EntityManager em = emf.createEntityManager();
+       
+       TAnagrafica r = new TAnagrafica();
+       
+       r.setNome("riccardo");
+       r.setCognome("cocciante");
+       
+       em.getTransaction().begin();
+       em.persist(r);
+       em.getTransaction().commit();
+       
+       em.close();
+       emf.close();
+       
+       
+       
+       
+    }
+}
